c4b8789487427a9b614417d31f94f20eb7f65957
Anna Marie Townsend <annamarie@Gaylanns-MacBook-Pro.local>
2019-04-03 16:22:39 -0400

    I added in all the files and fixed up the code

---
+++ instagram/homebrew/Library/Homebrew/test/cmd/pin_spec.rb
@@ -0,0 +1,13 @@
+require "cmd/shared_examples/args_parse"
+
+describe "Homebrew.pin_args" do
+  it_behaves_like "parseable arguments"
+end
+
+describe "brew pin", :integration_test do
+  it "pins a Formula's version" do
+    install_test_formula "testball"
+
+    expect { brew "pin", "testball" }.to be_a_success
+  end
+end
